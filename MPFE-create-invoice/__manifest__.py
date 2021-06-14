93194cf664e1a39e3c30332b835081874ba7f2e2
Silvio Benvegnù <silvio.benve@gmail.com>
2021-06-14 15:00:20 +0200

    new fields

--- MPFE-create-invoice/__manifest__.py
+++ MPFE-create-invoice/__manifest__.py
@@ -21,6 +21,9 @@ This module copy:
 - x_studio_incoterms from sale.order -> x_studio_incoterms in account.move
 - x_studio_metodo_di_pagamento_per_questo_ordine from sale.order -> x_studio_metodo_di_pagamento_per_questa_fattura in account.move
 - x_studio_da_n_pezzi from sale.order.line -> x_studio_da_n_pezzi in account.move.line
+- x_studio_da_cartoni from sale.order.line -> x_studio_da_cartoni in account.move.line
+- x_studio_da_pallet from sale.order.line -> x_studio_da_pallet in account.move.line
+- x_studio_supplier from sale.order.line -> x_studio_supplier in account.move.linecamp
 
 when an account_invoice is generated.  
 
